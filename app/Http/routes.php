5ab0bab9cdb148f36fa633c5c7b1552c2b166ce0
ihor <sup.on.php@gmail.com>
2018-05-29 19:12:38 +0300

    routing : index create store destroy

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,8 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
 Route::get('/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/tasks/create', 'TaskController@create')->name('tasks.create');
-Route::post('/task', 'TaskController@store')->name('tasks.store');
-Route::delete('/task/{task}', 'TaskController@destroy')->name('tasks.destroy');
+Route::post('/tasks', 'TaskController@store')->name('tasks.store');
+Route::delete('/tasks/{task}', 'TaskController@destroy')->name('tasks.destroy');
